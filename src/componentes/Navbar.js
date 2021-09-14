c764073b544d1e7d3f50e2e75c4c8eb264c89843
cauefrancisco <cauefranscis@gmail.com>
2021-09-14 17:02:36 -0300

    navbar

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -1,10 +1,29 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from './Button';
 
 
 function Navbar() {
+
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const [button, setButton] = useState(true)
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', showButton);
+
     return (
-       
+        <>
             <nav className="navbar">
                 <div className="navbar-container">
                     <Link to="/" className="navbar-logo">
@@ -13,9 +32,35 @@ function Navbar() {
                         Meus projetos literários
                     </h6>
                     </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/textos' className='nav-links' onClick={closeMobileMenu}>
+                                Textos
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/ebooks' className='nav-links' onClick={closeMobileMenu}>
+                                Ebooks
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                                Sign-up
+                            </Link>
+                        </li>
+                    </ul>
+                    {button && <Button buttonStyle='btn--ouline'>SIGN UP</Button>}
                 </div>
              </nav>
-        
+        </>
     )
 }
 
