5854f92f7b61d3c96f8fa4379ff33f32895a4ea3
cauefrancisco <cauefranscis@gmail.com>
2021-09-14 23:47:40 -0300

    navbar update

--- src/componentes/Button.js
+++ src/componentes/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Button.css';
 import { Link } from 'react-router-dom';
 
-const STYLES = ['btn--primary', 'btn--outline'];
+const STYLES = ['btn--primary', 'btn--outline', 'btn--test'];
 
 const SIZES = ['btn--medium', 'btn--large'];
 
@@ -27,7 +27,9 @@ export const Button = ({
             type={type}
             >
                 {children}
+
             </button>
+            
         </Link>
 
     )
--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -1,16 +1,18 @@
-import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import { Button } from './Button';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 
 function Navbar() {
 
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
 
-    const [button, setButton] = useState(true)
+   
 
     const showButton = () => {
         if(window.innerWidth <= 960) {
@@ -20,19 +22,20 @@ function Navbar() {
         }
     };
 
+    useEffect(() => {
+        showButton();
+    }, []);
+
     window.addEventListener('resize', showButton);
 
     return (
         <>
-            <nav className="navbar">
-                <div className="navbar-container">
+            <nav className='navbar'>
+                <div className='navbar-container'>
                     <Link to="/" className="navbar-logo">
-                    Quarto188 <i class="fas fa-book-reader" />
-                    <h6>
-                        Meus projetos literários
-                    </h6>
+                     <span> Estante 188 </span> 
                     </Link>
-                    <div className="menu-icon" onClick={handleClick}>
+                    <div className='menu-icon' onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
                     <ul className={click ? 'nav-menu active' : 'nav-menu'}>
